a3823ac7597c85b0178ca7dc220425f65c276f25
Lennex Zinyando <brizdigital@gmail.com>
2025-02-22 09:17:14 +0200

    Images url fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@mastra/pg"],
+  images: {
+    domains: ["i.kym-cdn.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.kym-cdn.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
