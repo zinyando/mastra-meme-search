1b49f0e916835697884646f3f2ae8295f9d56737
Lennex Zinyando <brizdigital@gmail.com>
2025-02-22 09:22:16 +0200

    More image fixes

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@mastra/pg"],
   images: {
-    domains: ["i.kym-cdn.com"],
+    domains: ["i.kym-cdn.com", "knowyourmeme.com"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "i.kym-cdn.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "knowyourmeme.com",
         pathname: "/**",
       },
     ],
